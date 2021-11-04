f3e131c9e5a708e63dbdb75091fb4a921fad9950
Vitali Nestsiaruk <agent708@gmail.com>
2021-11-05 00:57:57 +0300

    multistructure moduls(model)

---
+++ model/src/main/java/com/epam/brest/model/Order.java
@@ -0,0 +1,46 @@
+package com.epam.brest.model;
+
+import java.util.Date;
+
+public class Order {
+
+    private Integer orderId;
+
+    private String shipper;
+
+    private Double totalPrice;
+
+    private Date date;
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getShipper() {
+        return shipper;
+    }
+
+    public void setShipper(String shipper) {
+        this.shipper = shipper;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
---
+++ model/src/main/java/com/epam/brest/model/Product.java
@@ -0,0 +1,64 @@
+package com.epam.brest.model;
+
+public class Product {
+
+    private Integer productId;
+
+    private String name;
+
+    private String value;
+
+    private Integer quantity;
+
+    private Double pricePerItem;
+
+    private Integer orderId;
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPricePerItem() {
+        return pricePerItem;
+    }
+
+    public void setPricePerItem(Double pricePerItem) {
+        this.pricePerItem = pricePerItem;
+    }
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+}
