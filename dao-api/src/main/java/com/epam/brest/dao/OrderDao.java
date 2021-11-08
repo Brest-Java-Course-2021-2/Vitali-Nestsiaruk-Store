b3c69560259d5ec748790da1f04cf3b69e282d03
Vitali Nestsiaruk <agent708@gmail.com>
2021-11-09 00:35:50 +0300

    Add dao and dao-api moduls

---
+++ dao-api/src/main/java/com/epam/brest/dao/OrderDao.java
@@ -0,0 +1,16 @@
+package com.epam.brest.dao;
+
+import com.epam.brest.model.Order;
+
+import java.util.List;
+
+public interface OrderDao {
+
+    List<Order> findAll();
+
+    Integer create(Order order);
+
+    Integer update(Order order);
+
+    Integer delete(Integer orderId);
+}
---
+++ dao/src/main/java/com/epam/brest/dao/OrderDaoJDBCImpl.java
@@ -0,0 +1,28 @@
+package com.epam.brest.dao;
+
+import com.epam.brest.model.Order;
+
+import java.util.List;
+
+public class OrderDaoJDBCImpl implements OrderDao{
+
+    @Override
+    public List<Order> findAll() {
+        return null;
+    }
+
+    @Override
+    public Integer create(Order order) {
+        return null;
+    }
+
+    @Override
+    public Integer update(Order order) {
+        return null;
+    }
+
+    @Override
+    public Integer delete(Integer orderId) {
+        return null;
+    }
+}
